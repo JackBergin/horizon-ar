7ef3a1b1060ea7b096913bb6ee1068575a2b5ae7
JackBergin <jackcbergin@gmail.com>
2024-08-05 17:48:46 -0700

    Further improvements to app security

--- vite.config.prod.js
+++ vite.config.prod.js
@@ -4,13 +4,15 @@ import path from 'path';
 import fs from 'fs';
 
 const topLevelFiles = fs.readdirSync(path.resolve(__dirname)).filter(file => 
-  file !== 'node_modules' && file !== 'dist' && file !== 'vite.config.prod.js'
+  file !== 'node_modules' && file !== 'dist' && file !== 'vite.config.js'
 );
 
 export default defineConfig({
   root: '.', // Specify the root directory of your project
   build: {
     outDir: 'dist', // Output directory for the build
+    emptyOutDir: true, // Clean the output directory before building
+    sourcemap: true, // Include sourcemaps for debugging
   },
   plugins: [
     viteStaticCopy({
@@ -20,7 +22,16 @@ export default defineConfig({
       }))
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
     open: true, // Open the app in the browser on server start
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/server.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/server.crt')),
+    },
   },
 });
